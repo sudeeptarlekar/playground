cb3fd18b00dce432f46028a68afe9ebe6f61327a
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-01-28 10:57:53 +0100

    Add rust crate for testing
    Change-Id: Id05e2b06f05375ad6b9cc9d153b253f3295f0d70

--- main.rs
+++ main.rs
@@ -1,4 +1,11 @@
+use anyhow::Result;
+
 fn main() {
     println!("Hello, World!");
     println!("Welcome to Rust world!");
+}
+
+fn fetch_remote() -> Result<()> {
+    let repo = git2::Repository::open(".")?;
+    Ok(())
 }
