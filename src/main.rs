0e93a5dbb56ca880aeb3f2acde01a3af0cc346c2
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-04-08 13:53:22 +0200

    Refactor rust crate
    Change-Id: I4c49fabf69ef32beb6086a76a6f1cb3aac1eabf4

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-fn main() {
+use anyhow::Context;
+use anyhow::Result;
+use git2::Repository;
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+    let repo =
+        Repository::open(".").context("Current working directory is not a Git repository")?;
+    Ok(())
 }
 
 fn some_random_function() {
