4ce47a1047d7a3a0d9578bdc6891e8a6b5478480
sudeep.tarlekar <sudeep.tarlekar@accenture.com>
2025-04-11 09:16:57 +0200

    Printed target revision
    Change-Id: I22a0f9e7cac73a965e852f92e748cf1b8b68f349

--- dictionary/src/lib.rs
+++ dictionary/src/lib.rs
@@ -1,3 +1,5 @@
 pub const RIM_INFO: &str = ".riminfo";
 pub const GERRIT_URL: &str = "girrit.int.esrlabs.com";
 pub const TARGET_REVISION: &str = "TARGET_REVISION";
+pub const RIM_MSG: &str =
+    "DO not update this file; Updating content of this file may corrupt the file";
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Gerrit URL : {}", dictionary::GERRIT_URL);
+    println!("Target Revision : {}", dictionary::TARGET_REVISION);
+    println!("Rim msg: {}", dictionary::RIM_MSG);
 }
