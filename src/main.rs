7fcc32c106ef5b5bae687e385e6fdc3c708972f2
test.user <test.user@accenture.com>
2025-09-11 11:02:53 +0530

    Test commit: Merged by Shamita
    Change-Id: Ife1ed92c2e8601b1c0839cd847ed12695ca805d2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use anyhow::{Context, Result};
+use git2::{Remote, Repository};
+
 fn main() {
     println!("Hello, world!");
     println!("Lorem Ipsum.....");
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
     println!("Rim info file should start with {}", dictionary::RIM_INFO);
+}
+
+fn fetch_remote<'repo>(repo: &'repo Repository, url: &str) -> Result<Remote<'repo>> {
+    repo.remote_anonymous(url)
+        .context("Unable to create an anonymous remote from url")
 }
